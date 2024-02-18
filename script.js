0320bf1fd8015c3b08561ba499593cd8b29463b6
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-18 12:46:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const makeChange = (c) => {
-    const quarters = Math.floor(c/ 25);
-    return c%= 25;
-    const dimes = Math.floor(c/ 10);
-    return c%= 10;
+    const quarters = Math.floor(c / 25);
+    c %= 25;
+    const dimes = Math.floor(c / 10);
+    c %= 10;
     const nickels = Math.floor(c / 5);
-    return c %= 5;
+    c %= 5;
     const pennies = c;
     return { q: quarters, d: dimes, n: nickels, p: pennies };
 };
 
-Do not the change the code below
+// Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
